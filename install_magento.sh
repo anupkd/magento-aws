77fd2283685192d6348bca3aa01ab7b801d75c71
anu <anu.pkkd@gmail.com>
2020-03-31 11:30:36 +0400

    docker-compose

--- install_magento.sh
+++ install_magento.sh
@@ -9,5 +9,8 @@ sudo docker network create magento
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/anupkd/magento-aws/master/docker-compose.yml -o /tmp/docker-compose.yml
-sudo docker-compose -f /tmp/docker-compose.yml -d
-sudo docker run -dt --network magento -p 80:80 anupkkd/magento2:latest
+sudo curl -L https://raw.githubusercontent.com/anupkd/magento-aws/master/varnish/default.vcl -o /tmp/default.vcl
+sudo /usr/local/bin/docker-compose -f /tmp/docker-compose.yml -d
+sudo docker run --name apache2 -dt  --network magento -p 8080:80 anupkkd/magento2:latest
+sudo docker run --name varnish -dt --network magento -p 80:80 -v /tmp/default.vcl:/etc/varnish/default.vcl:ro --tmpfs /usr/local/var/varnish:e
+xec -d varnish
