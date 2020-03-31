19326d2c2d3e52161cb8ffdf9b27d7f0faf58573
anu <anu.pkkd@gmail.com>
2020-03-31 11:45:22 +0400

    varnish

--- install_magento.sh
+++ install_magento.sh
@@ -10,7 +10,7 @@ sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-c
 sudo chmod +x /usr/local/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/anupkd/magento-aws/master/docker-compose.yml -o /tmp/docker-compose.yml
 sudo curl -L https://raw.githubusercontent.com/anupkd/magento-aws/master/varnish/default.vcl -o /tmp/default.vcl
-sudo /usr/local/bin/docker-compose -f /tmp/docker-compose.yml -d
+sudo /usr/local/bin/docker-compose -f /tmp/docker-compose.yml up -d
 sudo docker run --name apache2 -dt  --network magento -p 8080:80 anupkkd/magento2:latest
 sudo docker run --name varnish -dt --network magento -p 80:80 -v /tmp/default.vcl:/etc/varnish/default.vcl:ro --tmpfs /usr/local/var/varnish:e
-xec -d varnish
+xec  varnish
